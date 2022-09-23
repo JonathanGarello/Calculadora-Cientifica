641db4d93a1ebf454bf328f9b4eedbe42755b4de
Jonathan Garello <jonathandanielgarello@gmail.com>
2022-09-23 10:14:09 -0300

    mejoras a la calculadora

--- src/main/java/com/github/sanchezih/calculadora/controller/CalculadoraAritmeticaController.java
+++ src/main/java/com/github/sanchezih/calculadora/controller/CalculadoraAritmeticaController.java
@@ -22,4 +22,38 @@ public class CalculadoraAritmeticaController {
 			@RequestParam("operando2") Integer operando2) {
 		return operando1 * operando2;
 	}
+	
+	@RequestMapping(value = "/factorial", method = RequestMethod.GET, params = { "operando1" })
+	public Integer factorial(@RequestParam("operando1") Integer operando1) {
+		Integer fact = 1;
+		for(int i = 1; i <= operando1; i++) {
+			fact*=i;
+		}
+		return fact;
+	}
+	
+	@RequestMapping(value = "/raiz", method = RequestMethod.GET, params = { "operando1" })
+	public Double raizCuadrada(@RequestParam("operando1") Integer operando1) {
+		Double resultado = Math.sqrt(operando1);
+		return resultado; 
+	}
+	
+	@RequestMapping(value = "/primo", method = RequestMethod.GET, params = { "operando1" })
+	public boolean primo(@RequestParam("operando1") Integer operando1) {
+		 if (operando1 == 0 || operando1 == 1 || operando1 == 4) {
+			    return false;
+			  }
+			  for (int x = 2; x < operando1 / 2; x++) {
+			    // Si es divisible por cualquiera de estos números, no
+			    // es primo
+			    if (operando1 % x == 0)
+			      return false;
+			  }
+			  // Si no se pudo dividir por ninguno de los de arriba, sí es primo
+			  return true;
+	}
+	
+	
+	
+	
 }
